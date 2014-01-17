ed1643a50634673b9778d27624a9976916ce519b
Alexis Rondeau <alexis.rondeau@gmail.com>
2014-01-17 17:00:30 -0500

    md

---
+++ spec/features/ecommerce_spec.rb
@@ -0,0 +1,7 @@
+# load libraries and DSL
+require "spec_helper"
+
+# what we're expecting this thing to do
+describe "Let People View a Product and Buy It", :type => :feature, :js => true do
+	#TBD
+end
---
+++ spec/features/landingpage_spec.rb
@@ -0,0 +1,7 @@
+# load libraries and DSL
+require "spec_helper"
+
+# what we're expecting this thing to do
+describe "Let People Read a Landingpage and Submit Their Email", :type => :feature, :js => true do
+	#TBD
+end
